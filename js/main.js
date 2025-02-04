c5f07ebd426209a98f0d98b257df4f97b4f19f34
Kev_CB <kevcb369@gmail.com>
2025-02-04 18:36:48 -0500

    Created cardItem with JS and information loaded

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const containerItem = document.querySelector(".card__container")
+
+async function loadItems(){
+    try {
+        const resp = await fetch('../data.json');
+        const items = await resp.json();
+        displayItems(items)
+        console.log(items)
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+}
+
+function displayItems(items){
+    const itemList = document.querySelector('.card__container');
+    itemsOrder = items.reverse()
+    itemsOrder.forEach( (item, index) => {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('item')
+      itemDiv.innerHTML = `
+        <h4><img src="${item.icon}" alt="Reaction icon">${item.category}</h4>
+        <p><strong>${item.score} </strong>/ 100</p>
+      `      
+      switch(item.category.toLowerCase()){
+        case 'reaction':
+            itemDiv.classList.add('itemReaction')
+            break;
+        case 'memory':
+            itemDiv.classList.add('itemMemory')
+            break
+        case 'verbal':
+            itemDiv.classList.add('itemVerbal')
+            break
+        case 'visual':
+            itemDiv.classList.add('itemVisual')
+            break
+      }
+          // Animación con retardo para simular orden descendente
+    setTimeout(() => {
+        itemList.appendChild(itemDiv);
+      }, (items.length - index - 1) * 0); //
+    })
+}
+loadItems()
+
